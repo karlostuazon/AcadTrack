32d131f12491e25b7ee93cf46f6f41bccf897668
karlostuazon <karlostuazon@gmail.com>
2022-01-12 17:38:00 +0800

    Update settings.py

--- AcadTrack/settings.py
+++ AcadTrack/settings.py
@@ -120,6 +120,7 @@ LOGIN_URL = 'login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'files')
 MEDIA_URL = '/files/'
